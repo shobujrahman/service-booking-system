2ed359be743b0d2634b2c59554994f5f49507360
prohor <isprohor007@gmail.com>
2025-08-02 13:58:47 +0600

    booking complete

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\Api\BookingController;
 use App\Http\Controllers\Api\ServiceController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
